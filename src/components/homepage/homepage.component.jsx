07b844f91e3bcb19cc4b5b0ec9404a798059f231
paolo1234 <paolocalamia49@gmail.com>
2020-07-30 17:06:02 +0200

    Created project with create-react-app and added files for homepage

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+
+    <div className='homepage'>
+
+        <div className='directory-menu'>
+
+            <div className="menu-item">
+
+                <div className="content">
+
+                    <h1 className='title'> Cane </h1>
+                    
+                    <span className="subtitle"> Brutto</span>
+                
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+
+                <div className="content">
+
+                    <h1 className='title'> Cane </h1>
+                    
+                    <span className="subtitle"> Brutto</span>
+                
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+
+                <div className="content">
+
+                    <h1 className='title'> Cane </h1>
+                    
+                    <span className="subtitle"> Brutto</span>
+                
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+
+                <div className="content">
+
+                    <h1 className='title'> Cane </h1>
+                    
+                    <span className="subtitle"> Brutto</span>
+                
+                </div>
+
+            </div>
+
+            <div className="menu-item">
+
+                <div className="content">
+
+                    <h1 className='title'> Cane </h1>
+                    
+                    <span className="subtitle"> Brutto</span>
+                
+                </div>
+
+            </div> 
+
+        </div>
+
+    </div>
+
+)
+
+export default HomePage;
